e5a573e0ab6f742d9e45156ae4da4c76b8880365
stkmukund <164354566+stkmukund@users.noreply.github.com>
2024-11-17 12:44:21 +0530

    signUpchanges

--- src/App.js
+++ src/App.js
@@ -1,89 +1,92 @@
-import React,{Suspense} from 'react'
+import React, { Suspense } from 'react'
 import './App.css';
 import { DataProvider } from './Context/Context';
 import Navbar from './Components/Navbar/Navbar';
-import {Routes,Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import ClipLoader from "react-spinners/ClipLoader";
 
-const Home = React.lazy(()=> import('./Components/Home/Home')) ;
-const About = React.lazy(()=> import('./Components/About/About')) ;
-const Careers = React.lazy(()=> import('./Components/Carrers/Careers'))
-const Safety = React.lazy(()=> import('./Components/Safety/Safety')) ;
-const Contact = React.lazy(()=> import('./Components/Contact/Contact')) ;
-const Login = React.lazy(()=> import('./Components/Login/Login')) ;
-const CaptainLogin = React.lazy(()=> import('./Components/Login/CaptainLogin')) ;
-const CustomerLogin = React.lazy(()=> import('./Components/Login/CustomerLogin')) ;
-const CaptainSignup = React.lazy(()=> import('./Components/Register/CaptainSignup')) ;
-const CustomerSignup = React.lazy(()=> import('./Components/Register/CustomerSignup')) ;
-const Register = React.lazy(()=> import('./Components/Register/Register')) ;
+const Home = React.lazy(() => import('./Components/Home/Home'));
+const About = React.lazy(() => import('./Components/About/About'));
+const Careers = React.lazy(() => import('./Components/Carrers/Careers'))
+const Safety = React.lazy(() => import('./Components/Safety/Safety'));
+const Contact = React.lazy(() => import('./Components/Contact/Contact'));
+const Login = React.lazy(() => import('./Components/Login/Login'));
+const CaptainLogin = React.lazy(() => import('./Components/Login/CaptainLogin'));
+const CustomerLogin = React.lazy(() => import('./Components/Login/CustomerLogin'));
+const CaptainSignup = React.lazy(() => import('./Components/Register/CaptainSignup'));
+const CustomerSignup = React.lazy(() => import('./Components/Register/CustomerSignup'));
+const Register = React.lazy(() => import('./Components/Register/Register'));
 
 function App() {
 
- return(
-  <div>
-   <DataProvider>
-     <Navbar/>
-     <Routes>
-      <Route path="/" element={<Suspense fallback={<div className='suspense'> <ClipLoader
-        color={"#f9c935"}
-    
-        aria-label="Loading Spinner"
-        data-testid="loader"
-      /></div>}> <Home /></Suspense>} />
-      <Route path="/about" element={<Suspense fallback={<div className='suspense'> <ClipLoader
-        color={"#f9c935"}
-        aria-label="Loading Spinner"
-        data-testid="loader"
-      /></div>}><About /></Suspense>} />
-      <Route path="/safety" element={<Suspense fallback={<div className='suspense'> <ClipLoader
-        color={"#f9c935"}
-        aria-label="Loading Spinner"
-        data-testid="loader"
-      /></div>}> <Safety /></Suspense>} />
-      <Route path="/careers" element={<Suspense fallback={<div className='suspense'> <ClipLoader
-        color={"#f9c935"}
-        aria-label="Loading Spinner"
-        data-testid="loader"
-      /></div>}> <Careers /></Suspense>} />
-      <Route path="/contact" element={<Suspense fallback={<div className='suspense'> <ClipLoader
-        color={"#f9c935"}
-        aria-label="Loading Spinner"
-        data-testid="loader"
-      /></div>}> <Contact /></Suspense>} />
-      <Route path="/login" element={<Suspense fallback={<div className='suspense'> <ClipLoader
-        color={"#f9c935"}
-        aria-label="Loading Spinner"
-        data-testid="loader"
-      /></div>}> <Login /></Suspense>} />
-      <Route path="/captainlogin" element={<Suspense fallback={<div className='suspense'> <ClipLoader
-        color={"#f9c935"}
-        aria-label="Loading Spinner"
-        data-testid="loader"
-      /></div>}> <CaptainLogin /></Suspense>} />
-      <Route path="/customerlogin" element={<Suspense fallback={<div className='suspense'> <ClipLoader
-        color={"#f9c935"}
-        aria-label="Loading Spinner"
-        data-testid="loader"
-      /></div>}> <CustomerLogin /></Suspense>} />
-      <Route path="/CaptainSignup" element={<Suspense fallback={<div className='suspense'> <ClipLoader
-        color={"#f9c935"}
-        aria-label="Loading Spinner"
-        data-testid="loader"
-      /></div>}> <CaptainSignup /></Suspense>} />
-      <Route path="/CustomerSignup" element={<Suspense fallback={<div className='suspense'> <ClipLoader
-        color={"#f9c935"}
-        aria-label="Loading Spinner"
-        data-testid="loader"
-      /></div>}> <CustomerSignup /></Suspense>} />
-      <Route path="/register" element={<Suspense fallback={<div className='suspense'> <ClipLoader 
-        color={"#f9c935"}
-        aria-label="Loading Spinner"
-        data-testid="loader"
-      /></div>}> <Register /></Suspense>} />
-     </Routes>
-   </DataProvider>
-  </div>
- )
+  return (
+    <div>
+      <DataProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Suspense fallback={<div className='suspense'> <ClipLoader
+            color={"#f9c935"}
+
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          /></div>}> <Home /></Suspense>} />
+          <Route path="/about" element={<Suspense fallback={<div className='suspense'> <ClipLoader
+            color={"#f9c935"}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          /></div>}><About /></Suspense>} />
+          <Route path="/safety" element={<Suspense fallback={<div className='suspense'> <ClipLoader
+            color={"#f9c935"}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          /></div>}> <Safety /></Suspense>} />
+          <Route path="/careers" element={<Suspense fallback={<div className='suspense'> <ClipLoader
+            color={"#f9c935"}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          /></div>}> <Careers /></Suspense>} />
+          <Route path="/contact" element={<Suspense fallback={<div className='suspense'> <ClipLoader
+            color={"#f9c935"}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          /></div>}> <Contact /></Suspense>} />
+          <Route path="/login" element={<Suspense fallback={<div className='suspense'> <ClipLoader
+            color={"#f9c935"}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          /></div>}> <Login /></Suspense>} />
+          <Route path="/captainlogin" element={<Suspense fallback={<div className='suspense'> <ClipLoader
+            color={"#f9c935"}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          /></div>}> <CaptainLogin /></Suspense>} />
+          <Route path="/customerlogin" element={<Suspense fallback={<div className='suspense'> <ClipLoader
+            color={"#f9c935"}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          /></div>}> <CustomerLogin /></Suspense>} />
+          <Route path="/CaptainSignup" element={<Suspense fallback={<div className='suspense'> <ClipLoader
+            color={"#f9c935"}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          /></div>}> <CaptainSignup /></Suspense>} />
+          <Route
+            path="/customer-signup"
+            element={
+              <Suspense fallback={<div className='suspense'> <ClipLoader color="#f9c935" /> </div>}>
+                <CustomerSignup isOpen={true} onClose={() => console.log('Closed')} />
+              </Suspense>
+            }
+          />
+          <Route path="/register" element={<Suspense fallback={<div className='suspense'> <ClipLoader
+            color={"#f9c935"}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          /></div>}> <Register /></Suspense>} />
+        </Routes>
+      </DataProvider>
+    </div>
+  )
 }
 
 export default App;
